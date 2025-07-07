613d6d5f9fe0aedb757ffdef13b3cadfc937af67
GschoesserPhilipp <philipp.gschoesser@gmail.com>
2025-07-07 09:17:17 +0200

    change package and project name

--- src/main.cpp
+++ src/main.cpp
@@ -6,16 +6,19 @@ int main(int argc, char *argv[])
 {
     QGuiApplication app(argc, argv);
 
-    qmlRegisterType<BleHandler>("Ble", 1, 0, "BleHandler");
+    qmlRegisterType<BleHandler>("feuerwehr_zeitmessung", 1, 0, "BleHandler");
 
     QQmlApplicationEngine engine;
-    QObject::connect(
-        &engine,
-        &QQmlApplicationEngine::objectCreationFailed,
-        &app,
-        []() { QCoreApplication::exit(-1); },
-        Qt::QueuedConnection);
-    engine.loadFromModule("ble_test", "Main");
+
+
+    const QUrl url("qrc:/feuerwehr_zeitmessung/qml/Main.qml");
+    QObject::connect(&engine, &QQmlApplicationEngine::objectCreated,
+                     &app, [url](QObject *obj, const QUrl &objUrl) {
+                         if (!obj && url == objUrl)
+                             QCoreApplication::exit(-1);
+                     }, Qt::QueuedConnection);
+
+    engine.load(url);
 
     return app.exec();
 }
